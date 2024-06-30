35190248a47fd67cd572be5dd00533ab1d6f0f24
Mohakg17 <146119834+Mohakg17@users.noreply.github.com>
2024-06-30 21:10:24 +0530

    Update chatGPT.js

--- frontend/src/apis/chatGPT/chatGPT.js
+++ frontend/src/apis/chatGPT/chatGPT.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const generateContentAPI = async (userPrompt) => {
   const response = await axios.post(
-    "http://localhost:8002/api/v1/openai/generate-content",
+    "https://contentcraft-backend.onrender.com/api/v1/openai/generate-content",
     {
       prompt: userPrompt,
     },
